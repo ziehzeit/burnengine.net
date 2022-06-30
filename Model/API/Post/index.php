cb06031de93b35086cf8302878f377885e7a845f
MaxBuhe01 <mb@shopmodule.com>
2022-06-30 23:45:24 +0200

    add dotEnv, fixed require and added use of namespace

--- Model/API/Post/index.php
+++ Model/API/Post/index.php
@@ -1,10 +1,16 @@
 <?php
+
+use Ziehzeit\Burnengine\Model\API\Post\Read;
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL & ~E_WARNING);
 
 
-require_once __DIR__.'/../../../../autoload.php';
+require_once __DIR__.'/../../../../../autoload.php';
 require_once __DIR__.'/../../../core.php';
 
-dumpvar(new \Ziehzeit\Burnengine\Model\API\Post\Read());
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../../../');
+$dotenv->load();
+
+new Read();
